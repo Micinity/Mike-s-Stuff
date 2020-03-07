035d07f0b9baf06aacdba9949e462ca47a1a39f3
Kroy <kroyacob@gmail.com>
2020-03-07 09:27:17 -0600

    Professionals have standards.

---
+++ BattleRoll.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+
+
+int main()
+{
+    srand(time(0));
+    string attack;
+    int hp = 100;
+    int atk = 5;
+    int atkRoll;
+    int enemyBlock;
+    int heroHP = 100;
+    int eatk = 5;
+
+
+    
+    while(hp > 0) {
+        cout << "Type punch, kick, or grab to attack." << endl;
+        cin >> attack;
+        cout << "You used: " << attack;
+        if(attack == "punch" || attack == "kick" || attack == "grab") {
+            atkRoll = 1 + (rand() % 20);
+            if(atkRoll <= 5) {
+                cout << "Attack Roll: " << atkRoll << "." << endl;
+                cout << "Your attack was too slow!" << endl;
+                cout << "EnemyName[HP: " << hp << "] I didn't even have to move for that one. Is that the best you can do?" << endl;
+            }
+
+            enemyBlock = 1 + (rand() % 20);
+
+            if(enemyBlock < atkRoll) {
+                if(atkRoll > 6 && atkRoll <= 10) {
+                    hp = hp - atk;
+                    cout << "Attack Roll: " << atkRoll << "." << endl;
+                    cout << "Dodge Roll: " << enemyBlock << "." << endl;
+                    cout << "You landed a hit!" << endl;
+                    cout << "EnemyName[HP: " << hp << "] Even a broken clock is right twice a day." << endl;
+                } if(atkRoll > 10 && atkRoll <= 15) {
+                    hp = hp - (atk + 5);
+                    cout << "Attack Roll: " << atkRoll << "." << endl;
+                    cout << "Dodge Roll: " << enemyBlock << "." << endl;
+                    cout << "You landed a strong hit! Extra damage!" << endl;
+                    cout << "EnemyName[HP: " << hp << "] I'll give you that one, kid." << endl;
+                } if(atkRoll > 15) {
+                    hp = hp - (atk + 10);
+                    cout << "Attack Roll: " << atkRoll << "." << endl;
+                    cout << "Dodge Roll: " << enemyBlock << "." << endl;
+                    cout << "Critical hit! The enemy is reeling!" << endl;
+                    cout << "EnemyName[HP: " << hp << "] Argh! I'll make you pay for this!" << endl;
+                }
+
+            }
+        }
+        if(enemyBlock == atkRoll) {
+            cout << "Attack Roll: " << atkRoll << "." << endl;
+            cout << "Dodge Roll: " << enemyBlock << "." << endl;
+            cout << "The enemy blocked your attack! They take reduced damage!" << endl;
+            hp = hp - (atk - 4);
+            cout << "EnemyName[HP: " << hp << "] Not bad, but you're going to have to try harder than that." << endl;
+        } if(enemyBlock > atkRoll) {
+            cout << "Attack Roll: " << atkRoll << "." << endl;
+            cout << "Dodge Roll: " << enemyBlock << "." << endl;
+            cout << "EnemyName[HP: " << hp << "] I am like water." << endl;
+        }
+    }
+    return 0;
+}
