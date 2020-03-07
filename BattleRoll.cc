b4289f4cb71421fdd092384bc38535cf8e58ea8e
Kroy <kroyacob@gmail.com>
2020-03-07 12:18:25 -0600

    Created display functions.

--- BattleRoll.cc
+++ BattleRoll.cc
@@ -9,6 +9,8 @@ using namespace std;
 
 //Prototypes
 string getNextAttack(vector<string> *attacks);
+void displayRolls(int atkRoll, int enemyBlock, string msg);
+void displayHp(int hp, string msg);
 
 int main()
 {
@@ -20,83 +22,87 @@ int main()
     int enemyBlock;
     int heroHP = 100;
     int eatk = 5;
-    vector<string> attacks = {"punch", "kick", "grab"};
+    vector<string> attacks = { "punch", "kick", "grab" };
 
     while(hp > 0) {
         attack = getNextAttack(&attacks);
         atkRoll = 1 + (rand() % 20);
         if(atkRoll <= 5) {
-            cout << "Attack Roll: " << atkRoll << "." << endl;
-            cout << "Your attack was too slow!" << endl;
-            cout << "EnemyName[HP: " << hp << "] I didn't even have to move for that one. Is that the best you can do?" << endl;
+            displayRolls(atkRoll, 0, "");
+            displayHp(hp, "I didn't even have to move for that one. Is that the best you can do?");
         }
 
         enemyBlock = 1 + (rand() % 20);
 
         if(enemyBlock < atkRoll) {
             if(atkRoll > 6 && atkRoll <= 10) {
-                hp = hp - atk;
-                cout << "Attack Roll: " << atkRoll << "." << endl;
-                cout << "Dodge Roll: " << enemyBlock << "." << endl;
-                cout << "You landed a hit!" << endl;
-                cout << "EnemyName[HP: " << hp << "] Even a broken clock is right twice a day." << endl;
+                hp -= atk;
+                displayRolls(atkRoll, enemyBlock, "You landed a hit!");
+                displayHp(hp, "Even a broken clock is right twice a day.");
             } if(atkRoll > 10 && atkRoll <= 15) {
-                hp = hp - (atk + 5);
-                cout << "Attack Roll: " << atkRoll << "." << endl;
-                cout << "Dodge Roll: " << enemyBlock << "." << endl;
-                cout << "You landed a strong hit! Extra damage!" << endl;
-                cout << "EnemyName[HP: " << hp << "] I'll give you that one, kid." << endl;
+                hp -=  (atk + 5);
+                displayRolls(atkRoll, enemyBlock, "You landed a strong hit! Extra damage!");
+                displayHp(hp, "I'll give you that one, kid.");
             } if(atkRoll > 15) {
-                hp = hp - (atk + 10);
-                cout << "Attack Roll: " << atkRoll << "." << endl;
-                cout << "Dodge Roll: " << enemyBlock << "." << endl;
-                cout << "Critical hit! The enemy is reeling!" << endl;
-                cout << "EnemyName[HP: " << hp << "] Argh! I'll make you pay for this!" << endl;
+                hp -= (atk + 10);
+                displayRolls(atkRoll, enemyBlock, "Critical hit! The enemy is reeling!");
+                displayHp(hp, "Argh! I'll make you pay for this!");
             }
-
         }
-        
+
         if(enemyBlock == atkRoll) {
-            cout << "Attack Roll: " << atkRoll << "." << endl;
-            cout << "Dodge Roll: " << enemyBlock << "." << endl;
-            cout << "The enemy blocked your attack! They take reduced damage!" << endl;
-            hp = hp - (atk - 4);
-            cout << "EnemyName[HP: " << hp << "] Not bad, but you're going to have to try harder than that." << endl;
+            hp -= (atk - 4);
+            displayRolls(atkRoll, enemyBlock, "The enemy blocked your attack! They take reduced damage!");
+            displayHp(hp, "Not bad, but you're going to have to try harder than that.");
         } if(enemyBlock > atkRoll) {
-            cout << "Attack Roll: " << atkRoll << "." << endl;
-            cout << "Dodge Roll: " << enemyBlock << "." << endl;
-            cout << "EnemyName[HP: " << hp << "] I am like water." << endl;
+            displayRolls(atkRoll, enemyBlock, "");
+            displayHp(hp, "I am like water.");
         }
     }
+    
     return 0;
 }
 
 string getNextAttack(vector<string> *attacks) {
-    string attack;
-    vector<string> atks = *attacks;
-    vector<string>::iterator it;
-    
+    vector<string> atkOpts = *attacks;
+
     //Generate prompt for attack input
     string prompt = "Type ";
-    int lenAtks = atks.size();
-    int i = 0;
-    for(string atk : atks) {
-        if(i < (lenAtks - 1)) {
-            prompt += atk + ", ";
+    int i = atkOpts.size();
+    for(string option : atkOpts) {
+        if(i > 1) {
+            prompt += option + ", ";
         } else {
-            prompt += "or " + atk;
+            prompt += "or " + option;
         }
-        i++;
+        i--;
     }
-
+    
+    string selAtk;
+    vector<string>::iterator it;
     while (true) {
         cout << prompt << endl;
-        cin >> attack;
+        cin >> selAtk;
 
-        it = find(atks.begin(), atks.end(), attack);
+        it = find(atkOpts.begin(), atkOpts.end(), selAtk);
         //We found a valid attack.
-        if(it != atks.end()) {
+        if(it != atkOpts.end()) {
             return *it;
+        } else {
+            cout << "Thats not an attack you fool!" << endl;
         }
     }
+}
+
+void displayRolls(int atkRoll, int enemyBlock, string msg) {
+        cout << "Attack Roll: " << atkRoll << "." << endl;
+        if(enemyBlock > 0) 
+            cout << "Dodge Roll: " << enemyBlock << "." << endl;
+        
+        if(msg != "")
+            cout << msg << endl;
+}
+
+void displayHp(int hp, string msg) {
+    cout << "EnemyName[HP: " << hp << "]" << msg << endl;   
 }
